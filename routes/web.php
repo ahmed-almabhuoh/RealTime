afce58a3c79a71d9b174583fbabbdf11ecdaae4c
Ahmed Almabhouh <az540546@gmail.com>
2025-01-02 18:35:40 +0200

    Pusher Public Channel - Corresponding Receiver

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Events\TestEvent;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('fire', function () {
+    // Broadcast(new TestEvent());
+    // dd('Here');
+
+    // broadcast(new TestEvent)->toOthers();
+    event(new TestEvent);
 });
